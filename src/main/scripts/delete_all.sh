961239b31768565ab2e39ac80ee0bf407037095c
sujitpal <sujit.pal@comcast.net>
2018-07-27 10:43:08 -0700

    Misc: added #!/bin/bash to script

--- src/main/scripts/delete_all.sh
+++ src/main/scripts/delete_all.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl "http://localhost:8983/solr/sodaindex/update?stream.body=<delete><query>*:*</query></delete>"
 curl "http://localhost:8983/solr/sodaindex/update?stream.body=<commit/>"
 
